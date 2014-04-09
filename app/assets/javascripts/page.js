73abf90ce3b993f057a901baf3a431217926e018
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-04-09 13:02:58 +0200

    Added fix for viewport height units in Safari.

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -7,7 +7,7 @@
 
       ga('create', 'UA-49872618-1', 'coldfrontconf.com');
       ga('send', 'pageview');
-  
+
     // Twitter
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');    
 
@@ -19,5 +19,7 @@
 	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1396360133947588";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
+
+    viewportUnitsBuggyfill.init();
 
 })();
