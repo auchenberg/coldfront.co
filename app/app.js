63dcb0762ec97b6261e0df0f1eb1bb12c8b94d8c
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-06-29 17:03:27 +0200

    Added schedule and introduced LESS.

--- app/app.js
+++ app/app.js
@@ -1,40 +1,49 @@
 var express = require('express');
 var app = express();
+var lessMiddleware = require('less-middleware');
+var autoprefixer = require('express-autoprefixer');
 
-var speakers = require('./speakers.json')
+var schedule = require('./schedule.json')
 
 app.configure(function(){
 
 	app.engine('ejs', require('ejs-locals'));
 
 	app.set('port', process.env.PORT || 8000);
-  	app.set('views', __dirname + '/views');
+    app.set('views', __dirname + '/views');
   	app.set('view engine', 'ejs');
 
   	app.use(express.bodyParser());
   	app.use(express.methodOverride());
-  	app.use('/assets', express.static(__dirname + '/assets/'));
+
+    app.use(lessMiddleware(__dirname + '/assets'));
+
+    app .use(autoprefixer({
+        options: 'last 2 versions'
+    }))
+
+    app.use(express.static(__dirname + '/assets'));
 
 });
 
 app.configure('development', function(){
- 	app.use(express.errorHandler({ 
- 		dumpExceptions: true, 
- 		showStack: true 
+ 	app.use(express.errorHandler({
+ 		dumpExceptions: true,
+ 		showStack: true
  	}));
 });
 
 app.get('/', function(req, res) {
  	res.render('index', {
   		_layoutFile: 'layout',
-  		speakers: speakers
+  		schedule: schedule
   	})
 });
 
 app.get('/speakers', function(req, res) {
   res.render('speakers', {
       _layoutFile: 'layout',
-      speakers: speakers
+      schedule: schedule
     })
 });
 
