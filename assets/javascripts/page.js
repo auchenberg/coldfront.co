077c5d5c4a7a10fe59d3e3891675bb3c5fe74820
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-02-15 10:18:46 +0100

    Extract scripts into its own js file.

---
+++ assets/javascripts/page.js
@@ -0,0 +1,27 @@
+(function() {
+
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-49872618-1', 'coldfrontconf.com');
+      ga('send', 'pageview');
+
+    // Twitter
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');    
+
+    // Faecbook
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1396360133947588";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+    if(/(iPhone|iPod|iPad).*AppleWebKit/i.test(navigator.userAgent)) {
+        document.documentElement.classList.add('safari')
+    }
+
+})();
