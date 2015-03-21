0d470a3133aaa998cc7b08c1f94dd60cf95de931
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-03-21 22:26:18 +0700

    Added Heroku vhost

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ server.set('port', process.env.PORT || 8000);
 server.use(vhost('2014.coldfrontconf.com', app2014))
 server.use(vhost('coldfrontconf.com', app));
 server.use(vhost('localhost', app));
+server.use(vhost('still-sands-3641.herokuapp.com', app));
 
 server.listen(server.get('port'), function(){
  	console.log("Express server listening on port %d in %s mode", server.get('port'), server.settings.env);
