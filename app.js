474ec52d7b5809951fd8c4a5f8db49996b279b1e
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-04-10 16:48:37 +0700

    Added caching headers on static assets.

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ server.set('views', __dirname + '/views');
 server.set('layout', 'default');
 server.use(expressLayouts)
 server.use(compression());
+
 server.use(lessMiddleware(__dirname + '/assets'));
 server.use(autoprefixer({
     options: 'last 2 versions'
 }));
-server.use(express.static(__dirname + '/assets'));
+
+server.use(express.static(__dirname + '/assets', {
+	maxAge: 86400000
+}));
 
 server.get('/', function(req, res) {
  	res.render('index', {
