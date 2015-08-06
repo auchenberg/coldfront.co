dc1a312550a1b72217c5d0be2ce358997b18a737
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-08-06 20:48:36 +0200

    Added schedule and new /speakers page

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-var express = require('express');
-var lessMiddleware = require('less-middleware');
-var autoprefixer = require('express-autoprefixer');
+var express = require('express')
+var lessMiddleware = require('less-middleware')
+var autoprefixer = require('express-autoprefixer')
 var expressLayouts = require('express-ejs-layouts')
 var compression = require('compression')
-var staticAsset = require('static-asset');
+var staticAsset = require('static-asset')
 
-var server = express();
+var server = express()
 
-server.set('view engine', 'ejs');
-server.set('views', __dirname + '/views');
-server.set('layout', 'default');
+server.set('view engine', 'ejs')
+server.set('views', __dirname + '/views')
+server.set('layout', 'default')
 server.use(expressLayouts)
-server.use(compression());
+server.use(compression())
 
-server.use(lessMiddleware(__dirname + '/assets'));
+server.use(lessMiddleware(__dirname + '/assets'))
 server.use(autoprefixer({
-    options: 'last 2 versions'
-}));
+  options: 'last 2 versions'
+}))
 
-server.use(staticAsset(__dirname + "/assets") );
-server.use(express.static(__dirname + '/assets'));
+server.use(staticAsset(__dirname + '/assets'))
+server.use(express.static(__dirname + '/assets'))
 
-server.get('/', function(req, res) {
- 	res.render('index', {
- 		schedule: require('./schedule'),
- 		pageClass: ''
- 	});
-});
+server.get('/', function (req, res) {
+  res.render('index', {
+    schedule: require('./schedule'),
+    pageClass: ''
+  })
+})
 
-server.get('/partners', function(req, res) {
-    res.render('partners', {
-    	pageClass: ''
-    })
-});
+server.get('/partners', function (req, res) {
+  res.render('partners', {
+    pageClass: ''
+  })
+})
 
-server.get('/workshop', function(req, res) {
-    res.render('workshop', {
-    	pageClass: 'workshop'
-    })
-});
+server.get('/workshop', function (req, res) {
+  res.render('workshop', {
+    pageClass: 'page-workshop'
+  })
+})
 
+server.get('/speakers', function (req, res) {
+  res.render('speakers', {
+    pageClass: 'page-speakers',
+    schedule: require('./schedule'),
+  })
+})
 
-module.exports = server;
+module.exports = server
