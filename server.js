64b35626d5b32ea98c16ecf18fe8a2f01fc0b433
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-02-01 09:00:23 -0800

    Add 2015 site

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-var express = require('express');
+var express = require('express')
 var vhost = require('vhost')
 
-var app = require("./app");
-var app2014 = require("./2014/app");
+var app = require('./app')
+var app2014 = require('./2014/app')
+var app2015 = require('./2015/app')
 
-var server = express();
-server.set('port', process.env.PORT || 8000);
+var server = express()
+server.set('port', process.env.PORT || 8000)
 
 server.use(vhost('2014.coldfrontconf.com', app2014))
-server.use(vhost('coldfrontconf.com', app));
-server.use(vhost('localhost', app));
-server.use(vhost('still-sands-3641.herokuapp.com', app));
+server.use(vhost('2015.coldfrontconf.com', app2015))
+server.use(vhost('coldfrontconf.com', app))
+server.use(vhost('localhost', app))
 
-server.listen(server.get('port'), function(){
- 	console.log("Express server listening on port %d in %s mode", server.get('port'), server.settings.env);
-});
+server.listen(server.get('port'), function () {
+  console.log('Express server listening on port %d in %s mode', server.get('port'), server.settings.env)
+})
