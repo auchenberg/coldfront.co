f13f256baaa3b8fc1bd056262a676ccb1d999144
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-03-18 22:03:57 -0700

    Initial work on 17.coldfront.co

---
+++ 17/app.js
@@ -0,0 +1,77 @@
+var express = require('express')
+var lessMiddleware = require('less-middleware')
+var autoprefixer = require('express-autoprefixer')
+var expressLayouts = require('express-ejs-layouts')
+var compression = require('compression')
+var staticAsset = require('static-asset')
+var browserify = require('browserify-middleware')
+
+var server = express()
+
+server.set('view engine', 'ejs')
+server.set('views', __dirname + '/views')
+server.set('layout', 'default')
+server.use(expressLayouts)
+server.use(compression())
+
+server.use(lessMiddleware(__dirname + '/assets'))
+server.use(autoprefixer({
+  options: 'last 2 versions'
+}))
+
+server.get('/bundle.js', browserify(__dirname + '/assets/javascripts/app.js', {
+  cache: true,
+  precompile: true
+}))
+
+server.use(staticAsset(__dirname + '/assets'))
+server.use(express.static(__dirname + '/assets'))
+
+server.get('/', function (req, res) {
+  res.render('index', {
+    speakers: require('./speakers'),
+    schedule: require('./schedule'),
+    pageClass: ''
+  })
+})
+
+server.get('/partners', function (req, res) {
+  res.render('partners', {
+    pageClass: ''
+  })
+})
+
+server.get('/diversity', function (req, res) {
+	res.render('diversity', {
+		pageClass: 'page-diversity'
+	})
+})
+
+server.get('/workshops', function (req, res) {
+  res.render('workshops', {
+    pageClass: 'page-workshops'
+  })
+})
+
+server.get('/roadbook', function (req, res) {
+  res.render('roadbook', {
+    pageClass: 'page-roadbook'
+  })
+})
+
+server.get('/live', function (req, res) {
+  res.render('live', {
+    pageClass: 'page-live',
+    noFooter: true,
+    noHeader: true
+  })
+})
+
+server.get('/speakers', function (req, res) {
+  res.render('speakers', {
+    pageClass: 'page-speakers',
+    speakers: require('./speakers'),
+  })
+})
+
+module.exports = server
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var vhost = require('vhost')
 var app2014 = require('./2014/app')
 var app2015 = require('./2015/app')
 var app2016 = require('./2016/app')
+var app17 = require('./17/app')
 var appSplash = require('./splash/app')
 
 var redirect = express()
 
 redirect.get('/', function (req, res) {
- res.redirect(301, 'https://2017.coldfront.co')
+  res.redirect(301, 'https://2017.coldfront.co')
 })
 
 var server = express()
@@ -19,8 +20,9 @@ server.use(vhost('2014.coldfront.co', app2014))
 server.use(vhost('2015.coldfront.co', app2015))
 server.use(vhost('2016.coldfront.co', app2016))
 server.use(vhost('2017.coldfront.co', appSplash))
+server.use(vhost('17.coldfront.co', app17))
 server.use(vhost('coldfront.co', redirect))
-server.use(vhost('localhost', app2016))
+server.use(vhost('localhost', app17))
 
 server.listen(server.get('port'), function () {
   console.log('Express server listening on port %d in %s mode', server.get('port'), server.settings.env)
