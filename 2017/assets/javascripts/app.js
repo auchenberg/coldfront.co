6dbc1402da8c15636757fa77ea6d321d72fd81c9
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-05-22 21:05:31 -0700

    Add a little mouse animation

--- 2017/assets/javascripts/app.js
+++ 2017/assets/javascripts/app.js
@@ -17,3 +17,20 @@ ga('create', 'UA-49872618-1', 'coldfront.co')
 ga('send', 'pageview')
 
 AOS.init();
+
+
+document.querySelectorAll('.movable').forEach((elm) => {
+  
+  elm.addEventListener('mousemove', (e) => {
+    var screenWidth = document.body.clientWidth / 2
+    var screenHeight = document.body.clientHeight / 2
+
+    let centroX = e.clientX - window.innerWidth / 2
+    let centroY = window.innerHeight / 2 - (e.clientY + 100)
+    let degX = centroX * .10
+    let degY = centroY * .10
+
+    e.target.style['transform'] = 'perspective(600px)' + 'rotateY('+ degX +'deg)  rotateX('+ degY +'deg)'    
+  })
+  
+})
